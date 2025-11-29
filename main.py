9ff0cb7da5a9220f9849d302b70f29029b582bce
Simon <bbpipibear@gmail.com>
2025-11-29 10:03:50 -0500

    use a different model

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ class InputText(BaseModel):
     text: str
 
 # --- Load ALBERT SST-2 model ---
-model_name = "textattack/albert-base-v2-SST-2"   # <-- KEEP THIS EXACT
+model_name = "finiteautomata/bertweet-base-sentiment-analysis"   # <-- KEEP THIS EXACT
 
 # Force slow tokenizer to avoid fast-tokenizer errors
 tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=False)
